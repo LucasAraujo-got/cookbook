6070bf396eacedcb9554d1a9964913a7cf831247
Lucas <lucas0714araujo@gmail.com>
2023-02-11 14:31:32 -0300

    adicionando estruturas condicionais

--- coockbook.rb
+++ coockbook.rb
@@ -1,18 +1,35 @@
-puts"Bem vindo ao CookBook, sua rede social de receitas!"
+puts"Bem vindo ao Cookbook, sua rede social de receitas!"
 
 receitas = []
 
-while(true) do
-    puts"Digite o nome da receita:"
-    nome = gets.chomp()
+puts"[1] Para cadastrar uma receita."
+puts"[2] Ver todas as receitas."
+puts"[3] Sair"
 
-    receitas << nome
+print "Escolha uma opção: "
+opcao = gets.to_i()
 
-    puts
-    puts"Receita #{nome} cadastrada com sucesso!"
-    puts
-    puts"======== Receitas cadastradas com sucesso! ========"
-    puts receitas
-    puts
+while(opcao != 3) do
+    if(opcao == 1)
+        puts"Digite o nome da receita:"
+        nome = gets.chomp()
+        receitas << nome
+        puts
+        puts"Receita #{nome} cadastrada com sucesso!"
+        puts
+    elsif(opcao == 2)
+        puts"======== Receitas cadastradas com sucesso! ========"
+        puts receitas
+        puts
+    else
+        puts "Opção inválida"
+    end
+    puts"[1] Para cadastrar uma receita."
+    puts"[2] Ver todas as receitas."
+    puts"[3] Sair"
 
+    print "Escolha uma opção: "
+    opcao = gets.to_i()
 end
+
+puts "Obrigado por usar o Cookbook, Até logo!"
